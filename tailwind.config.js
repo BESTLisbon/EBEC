cf63aa471ab1102c95478d451f9cada52ada5452
Hugo Pereira <hugo00pereira@gmail.com>
2022-12-08 17:21:41 +0000

    Canvas works, baby!

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 module.exports = {
     content: ["./public/**/*.{html,js}"],
     theme: {
+      fontFamily: {
+        'bnmachine': ['BN Machine', 'sans-serif'],
+        'typemachine': ['Type Machine', 'sans-serif']
+      }
     },
     plugins: [],
 };
